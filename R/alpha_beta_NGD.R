38eb94cbfc12f980d087fd034af4a0db33bef326
Arpan Ghosh <arpan2001@tamu.edu>
2024-11-06 01:12:57 -0600

    Input-output config & basic function structure

---
+++ R/alpha_beta_NGD.R
@@ -0,0 +1,24 @@
+#(α, β)-Normalized Gradient Descent (NGD)
+#This algorithm is particularly for α-strongly convex and β-smooth functions.
+
+#Inputs
+#x - initial starting point on R^d
+#D - distance between x_1 & optimial x
+#K_{\epsilon} - phase counts / the no of phases needed
+#T_{\epsilon} - Query budget
+#\eta - learning rate (step size) to control how much the point updates each iteration.
+
+
+alpha_beta_NGD <- function(initial_point, alpha, beta, tolerance, max_iter = 1000) {
+
+
+  # Main loop for phases
+  for (k in 1:phase_count) {
+
+  }
+
+  return(x)
+}
+
+.
+
