ec9eb986a48f35d471ae99828f081723bb4d9162
Arpan Ghosh <arpan2001@tamu.edu>
2024-11-06 00:50:31 -0600

    Input config

---
+++ R/beta_Normalized_Gradient.R
@@ -0,0 +1,4 @@
+#x_1 -intial d-dimentional intial point
+#\eta - learning rate (step size) to control how much the point updates each iteration.
+#\gamma - (peterbation parameter) used to generate nearby points to probe the function's landscape.
+# T - maximum number of steps the algorithm will take.
