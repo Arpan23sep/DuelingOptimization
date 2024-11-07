d42b800213b6ed523a7bbe6c93cef757d7323afa
Arpan Ghosh <arpan2001@tamu.edu>
2024-11-06 21:10:54 -0600

    Function to compare two values

--- R/beta_Normalized_Gradient.R
+++ R/beta_Normalized_Gradient.R
@@ -2,3 +2,9 @@
 #\eta - learning rate (step size) to control how much the point updates each iteration.
 #\gamma - (peterbation parameter) used to generate nearby points to probe the function's landscape.
 # T - maximum number of steps the algorithm will take.
+
+compare_points <- function(x1, x2) {
+  # This function should return +1 if f(x1) < f(x2), -1 otherwise
+  # Replace this with real preference feedback or a function proxy.
+  return(sample(c(-1, 1), 1))  # Random feedback for illustration
+}
