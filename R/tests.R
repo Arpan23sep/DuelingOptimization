bdd891a343dccb4d846cef7f20b25d2d9cf05dbd
Arpan Ghosh <arpan2001@tamu.edu>
2024-11-07 22:21:20 -0600

    alpha_beta_ngd function testing

--- R/tests.R
+++ R/tests.R
@@ -9,3 +9,17 @@ T <- 100
 # Run the normalized gradient descent
 optimal_point <- beta_NGD(initial_point, eta, gamma, T)
 print(optimal_point)
+
+
+#Basic Example for alpha_beta_NGD
+
+# Set up parameters for strongly convex optimization
+initial_point <- c(1, 1)
+alpha <- 0.1
+beta <- 0.5
+tolerance <- 1e-4
+max_iter <- 500
+
+# Run the phase-based gradient descent
+optimal_point <- alpha_beta_NGD(initial_point, alpha, beta, tolerance, max_iter)
+print(optimal_point)
