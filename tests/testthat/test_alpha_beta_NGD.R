eb767b9086a610d85610d25f7a996dbde8ba24be
Arpan Ghosh <arpan2001@tamu.edu>
2024-11-27 23:24:48 -0600

    Test function for alpha_beta_NGD

---
+++ tests/testthat/test_alpha_beta_NGD.R
@@ -0,0 +1,29 @@
+test_that("alpha_beta_NGD outputs a valid solution", {
+  # Define a simple quadratic objective function
+  f <- function(x) {
+    sum((x - 3)^2)
+  }
+
+  # Set parameters for testing
+  initial_point <- c(0, 0)
+  alpha <- 1
+  beta <- 1
+  D <- 10
+  tolerance <- 0.1
+
+  # Call the alpha_beta_NGD function
+  result <- alpha_beta_NGD(initial_point, alpha, beta, D, tolerance, f)
+
+  # Check that the result is a numeric vector
+  expect_type(result, "double")
+
+  # Check that the result has the same length as the initial point
+  expect_equal(length(result), length(initial_point))
+
+  # Check that the function value at the result is close to the minimum
+  final_value <- f(result)
+  expect_true(final_value < f(initial_point))
+
+  # Check that the result is close to the known minimizer
+  expect_true(max(abs(result - 3)) < tolerance)
+})
